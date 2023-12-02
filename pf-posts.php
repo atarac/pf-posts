28bf9b69cb34d776b0eac4503565ea842bcf09ea
atarac <atara.c.daisuke@gmail.com>
2023-12-02 22:16:57 +0900

    機能追加：投稿日・更新日表示、表示オプション、デザイン切り替え機能

--- pf-posts.php
+++ pf-posts.php
@@ -18,7 +18,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 function my_dynamic_block_render_callback($attributes) {
 	$numberOfPosts = isset($attributes['numberOfPosts']) ? $attributes['numberOfPosts'] : 3;
-	$selectedPostType = isset($attributes['selectedPostType']) ? $attributes['selectedPostType'] : 'news';
+	$selectedPostType = isset($attributes['selectedPostType']) ? $attributes['selectedPostType'] : 'blog';
+	$isShowDate = isset($attributes['isShowDate']) ? $attributes['isShowDate'] : true;
+	$isShowModified = isset($attributes['isShowModified']) ? $attributes['isShowModified'] : false;
+	$isShowContent = isset($attributes['isShowContent']) ? $attributes['isShowContent'] : true;
+	$letterCounts = isset($attributes['letterCounts']) ? $attributes['letterCounts'] : 50;
+	$postDesign = isset($attributes['postDesign']) ? $attributes['postDesign']: 'post-design1';
 
 	if (!post_type_exists($selectedPostType)) {
 			return '<div class="custom-message"><p>表示できるカテゴリーがありません。</p></div>';
@@ -31,15 +36,35 @@ function my_dynamic_block_render_callback($attributes) {
 	$query = new WP_Query($args);
 
 	if (!$query->have_posts()) {
-			return '<p>投稿がありません。</p>';
+			return '<div class="custom-message"><p>投稿がありません。</p></div>';
 	}
 
-	$output = '<div class="wp-block-create-block-pf-posts">';
-	while ($query->have_posts()) {
+	$output = '<div class="wp-block-create-block-pf-posts ' . $postDesign . '">';
+		while ($query->have_posts()) {
 			$query->the_post();
-			$output .= '<h3><a href="' . get_permalink() . '">' . get_the_title() . '</a></h3>';
-			$output .= '<p>' . get_the_excerpt() . '</p>';
-	}
+			$output .= '<div class="post-container">';
+			$output .= '<h3 class="post-title">';
+			$output .= '<a href="' . get_permalink() . '">' . get_the_title() . '</a></h3>';
+		
+			if ($isShowDate) {
+				$output .= '<p class="post-day">投稿日：' . get_the_date('Y年n月j日') . '</p>';
+			}
+
+			if ($isShowModified) {
+				$output .= '<p class="modified-day">更新日：' . get_the_modified_date('Y年n月j日') . '</p>';
+			}
+
+			if ($isShowContent) {
+				$content = get_the_content();
+				$content = strip_tags($content);
+				$content = trim($content);
+				$content = (mb_strlen($content) > $letterCounts) ? mb_substr($content, 0, $letterCounts) . '...' : $content;
+				$output .= '<p class="post-content">' . $content . '</p>';
+			}
+
+			$output .= '</div>';
+		}
+
 	$output .= '</div>';
 
 	wp_reset_postdata();
--- src/edit.js
+++ src/edit.js
@@ -1,32 +1,73 @@
 import { useSelect } from '@wordpress/data';
 import React, { useState, useEffect } from 'react';
 import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
-import { RangeControl, SelectControl, Spinner } from '@wordpress/components';
+import {RangeControl, SelectControl, ToggleControl, Spinner, PanelBody } from '@wordpress/components';
 
-const RenderPosts = ({ posts, postTypes }) => {
+const formatDate = (dateString) => {
+	const date = new Date(dateString);
+	return date.toLocaleDateString('ja-JP', {
+		year:'numeric',
+		month: 'long',
+		day: 'numeric'
+	});
+};
+
+const truncateText = (text, number) => {
+	const cleanedText = text.replace(/<[^>]+>/g, '').trimStart().trimEnd();
+	const truncatedText = cleanedText.length > number
+		? cleanedText.substring(0, number + 1) + '...'
+		: cleanedText;
+	return truncatedText;
+};
+ 
+const RenderPosts = ({
+	posts,
+	postTypes,
+	isShowDate,
+	isShowModified,
+	isShowContent,
+	letterCounts,
+ }) => {
 	if (!postTypes || !postTypes.length) {
 		return (
 			<div className="custom-message">
 				<p>表示できるカテゴリーがありません。</p>
 			</div>
 		);
-	}
+	};
 
-	if (!posts || posts.length === 0) {
-		return <p>投稿がありません。</p>;
-	}
+	if (!posts || !posts.length) {
+		return (
+			<div className="custom-message">
+				<p>投稿がありません。</p>
+			</div>
+		);
+	};
 
-	return posts.map(post => (
-		<div key={post.id}>
-			<h3><a href={post.link}>{post.title.rendered}</a></h3>
-			<p>{post.content.rendered.replace(/<[^>]+>/g, '').substring(0, 55)}</p>
+	return posts.map((post) => (
+		<div key={ post.id } className="post-container">
+			<h3 className="post-title"><a href={ post.link }>{ post.title.rendered }</a></h3>
+			{ isShowDate && <p className="post-day">投稿日：{ formatDate(post.date) }</p> }
+			{ isShowModified && <p className="modified-day">更新日：{ formatDate(post.modified) }</p> }
+			{ isShowContent && <p className="post-content">{ truncateText(post.content.rendered, letterCounts) }</p> }
 		</div>
 	));
 };
 
 const Edit = ({ attributes, setAttributes }) => {
-	const { numberOfPosts, selectedPostType } = attributes;
-	const blockProps = useBlockProps();
+	const {
+		numberOfPosts, 
+		selectedPostType,
+		isShowDate,
+		isShowModified,
+		isShowContent,
+		letterCounts,
+		postDesign
+	} = attributes;
+
+	const blockProps = useBlockProps({
+		className: postDesign
+	});
 
 	const excludedPostTypes = [
 		'post',
@@ -40,15 +81,20 @@ const Edit = ({ attributes, setAttributes }) => {
 		'modal'
 	];
 
-	const postTypes = useSelect( (select) => {
+	const postTypes = useSelect((select) => {
 		const allPostTypes = select('core').getPostTypes({ per_page: -1 });
 		if (!allPostTypes) return [];
-			return allPostTypes.filter(postType => !excludedPostTypes.includes(postType.slug));
+			return allPostTypes.filter((postType) => !excludedPostTypes.includes(postType.slug));
 	}, []);
 
-	const posts = useSelect( (select) => {
+	const posts = useSelect((select) => {
 		return select('core').getEntityRecords('postType', selectedPostType, { per_page: numberOfPosts });
-	}, [ selectedPostType, numberOfPosts ]);
+	}, [selectedPostType, numberOfPosts]);
+
+	const designOption = [
+		{ label: 'デザイン1', value:'post-design1' },
+		{ label: 'デザイン2', value:'post-design2' },
+	]
 
 	const [loading, setLoadeing] = useState(true);
 
@@ -59,35 +105,72 @@ const Edit = ({ attributes, setAttributes }) => {
 		return () => clearTimeout(timer)
 	}, [postTypes, posts]);
 
-	console.log("postTypes", postTypes);
-	console.log("posts",posts);
-
 	if (loading) {
 		return <Spinner />;
 	};
 
 	return (
-		<div {...blockProps}>
-			{!postTypes || postTypes.length > 0 && (
+		<div { ...blockProps }>
+			{ !postTypes || postTypes.length > 0 && (
 				<InspectorControls>
-					<SelectControl
-						label = "投稿タイプの選択"
-						value={ selectedPostType }
-						options={ postTypes.map((type) => ({ label: type.name, value: type.slug })) }
-						onChange={ (newType) => setAttributes({ selectedPostType: newType }) }
-					/>
-					<RangeControl
-						label="表示する投稿の数"
-						value={ numberOfPosts }
-						onChange={ (newNumber) => setAttributes({ numberOfPosts: newNumber }) }
-						min={1}
-						max={5}
-					/>
+					<PanelBody title="表示内容の設定">
+						<SelectControl
+							label = "カテゴリー選択"
+							value={ selectedPostType }
+							options={ postTypes.map((type) => ({ label: type.name, value: type.slug })) }
+							onChange={ (newType) => setAttributes({ selectedPostType: newType }) }
+						/>
+						<RangeControl
+							label="表示する投稿の数"
+							value={ numberOfPosts }
+							onChange={ (newNumber) => setAttributes({ numberOfPosts: newNumber }) }
+							min={1}
+							max={5}
+						/>
+						<p>表示オプション</p>
+						<ToggleControl
+							label="投稿日の表示"
+							checked={ isShowDate }
+							onChange={ () => setAttributes({ isShowDate: !isShowDate }) }
+						/>
+						<ToggleControl
+							label="更新日の表示"
+							checked={ isShowModified }
+							onChange={ () => setAttributes({ isShowModified: !isShowModified }) }
+						/>
+						<ToggleControl
+							label="投稿内容の表示"
+							checked={ isShowContent }
+							onChange={ () => setAttributes({ isShowContent: !isShowContent }) }
+						/>
+						{ isShowContent &&
+							<RangeControl
+								label="投稿内容の表示文字数"
+								value={ letterCounts }
+								onChange={ (newLetterCounts) => setAttributes({ letterCounts: newLetterCounts }) }
+								min={1}
+								max={50}
+						  />
+						}
+						<SelectControl
+							label = "デザイン設定"
+							value={ postDesign }
+							options={ designOption }
+							onChange={ (newDesign) => setAttributes({ postDesign: newDesign }) }
+						/>
+					</PanelBody>
 				</InspectorControls>
 			)}
-				<RenderPosts posts={posts} postTypes={postTypes} />
-			</div>
-    );
+				<RenderPosts
+					posts={ posts }
+					postTypes={ postTypes }
+					isShowDate={ isShowDate }
+					isShowModified={ isShowModified }
+					isShowContent={ isShowContent }
+					letterCounts={ letterCounts }
+				/>
+		</div>
+  );
 };
 
 export default Edit;
